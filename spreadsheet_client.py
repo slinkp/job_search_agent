691b75a982549844c11427257e01d41d6893a4ad
Paul Winkler <slinkp@gmail.com>
2025-04-16 11:37:22 -0400

    Remove some copy-pasted garbage

--- spreadsheet_client.py
+++ spreadsheet_client.py
@@ -30,18 +30,6 @@ FIRST_DATA_ROW = 2  # 0-indexed
 # Configure logging
 logger = logging.getLogger(__name__)
 
-from google.auth.exceptions import RefreshError
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-
-
-FIRST_DATA_ROW = 2  # 0-indexed
-
 
 @functools.cache
 def authorize() -> Credentials:
