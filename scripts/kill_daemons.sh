73dd083f80d30a0aa42492127e14e38a4abbc317
Paul Winkler <slinkp@gmail.com>
2025-08-17 21:12:42 -0400

    Add shell script to kill stale daemons

---
+++ scripts/kill_daemons.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PIDS=$(ps wax | grep research_daemon | grep ython | grep -v grep | cut -d ' ' -f 1)
+if [ -z "$PIDS" ]; then
+	echo None to kill
+else
+	for P in `echo "$PIDS"`; do
+		echo "Killing $P ..."
+		kill $P
+	done
+	echo "done"
+fi
